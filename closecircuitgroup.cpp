083e3b3b470cc2e5087f305fe0f6ab98b115fcf9
Edward Lisovtsev <edward.lisovtsev@gmail.com>
2016-02-19 14:47:44 +0300

    НЗЦг в ручном, в начальном варианте. Сделал на кнопке Пуск НЦЗг слот по умолчанию, перетащил реализацию в отдельный файл closecircuitgroup.cpp

---
+++ closecircuitgroup.cpp
@@ -0,0 +1,359 @@
+#include <QDebug>
+#include <QMessageBox>
+#include <QDateTime>
+
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "battery.h"
+
+extern QVector<Battery> battery;
+
+// Нажата кнопка проверки напряжения замкнутых цепей групп
+void MainWindow::on_btnClosedCircuitVoltageGroup_clicked()
+{
+    qDebug()<<"checkClosedCircuitVoltageGroup";
+    quint16 codeADC=0; // принятый код АЦП
+    float fU=0; // принятое напряжение в вольтах
+    // код порогового напряжения = пороговое напряжение В / коэфф. (вес разряда) + смещение (в коде)
+    quint16 codeLimit=settings.closecircuitgroup_limit/settings.coefADC1 + settings.offsetADC1; // код, пороговое напряжение.
+    int ret=0; // код возврата ошибки
+    QString str_num; // номер цепи
+    QDateTime starttime; // время начала измерения
+    QDateTime dt; // текущее время очередного измерения
+    double x; // текущая координата Х
+    int cycleTimeSec=settings.time_depassivation[2]; // длительность цикла проверки в секундах
+
+    if(loop.isRunning()){qDebug()<<"loop.isRunning()!"; return;} // костыль: если цикл уже работает - выйти обратно
+    timerPing->stop(); // остановить пинг
+
+    baSendArray.clear();
+    baSendCommand.clear();
+    baRecvArray.clear();
+
+    int h = 45; //Шаг, с которым будем пробегать по оси Ox
+    //double x, y;
+    ui->widgetClosedCircuitVoltageGroup->addGraph(); // blue line
+    ui->widgetClosedCircuitVoltageGroup->graph(0)->setPen(QPen(Qt::blue));
+    ui->widgetClosedCircuitVoltageGroup->graph(0)->clearData();
+    ui->widgetClosedCircuitVoltageGroup->addGraph(); // blue dot
+    ui->widgetClosedCircuitVoltageGroup->graph(1)->clearData();
+    ui->widgetClosedCircuitVoltageGroup->graph(1)->setLineStyle(QCPGraph::lsNone);
+    //ui->widgetClosedCircuitVoltageGroup->graph(1)->setPen(QPen(Qt::green));
+    ui->widgetClosedCircuitVoltageGroup->graph(1)->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssCircle, Qt::blue, Qt::white, 7));
+    ui->widgetClosedCircuitVoltageGroup->addGraph(); // red line
+    ui->widgetClosedCircuitVoltageGroup->graph(2)->setPen(QPen(Qt::red));
+    ui->widgetClosedCircuitVoltageGroup->graph(2)->setBrush(QBrush(QColor(255, 0, 0, 20)));
+    ui->widgetClosedCircuitVoltageGroup->graph(2)->clearData();
+    ui->widgetClosedCircuitVoltageGroup->graph(2)->addData(0, settings.closecircuitgroup_limit);
+    ui->widgetClosedCircuitVoltageGroup->graph(2)->addData(cycleTimeSec, settings.closecircuitgroup_limit);
+
+    ui->widgetClosedCircuitVoltageGroup->xAxis->setLabel(tr("Время, c"));
+    ui->widgetClosedCircuitVoltageGroup->xAxis->setRange(0, cycleTimeSec);
+    ui->widgetClosedCircuitVoltageGroup->yAxis->setLabel(tr("Напряжение, В"));
+    ui->widgetClosedCircuitVoltageGroup->yAxis->setRange(20, 33);
+
+    if (((QPushButton*)sender())->objectName() == "btnClosedCircuitVoltageGroup") {
+        iStepClosedCircuitVoltageGroup = 1;
+        bPause = false;
+        ui->btnClosedCircuitVoltageGroup_2->setEnabled(false);
+    }
+    if (((QPushButton*)sender())->objectName() == "btnClosedCircuitVoltageGroup_2")
+        bPause = false;
+    if (bPause) return;
+    ui->groupBoxCOMPort->setEnabled(false);
+    ui->groupBoxDiagnosticDevice->setEnabled(false);
+    ui->groupBoxDiagnosticMode->setEnabled(false);
+    ui->tabWidget->addTab(ui->tabClosedCircuitVoltageGroup, ui->rbClosedCircuitVoltageGroup->text());
+    ui->tabWidget->setCurrentIndex(ui->tabWidget->count()-1);
+    Log(tr("Проверка начата - %1").arg(ui->rbClosedCircuitVoltageGroup->text()), "blue");
+
+    // сбросить коробочку
+    baSendArray = (baSendCommand="IDLE")+"#"; // подготовить буфер для передачи
+    sendSerialData(); // послать baSendArray в порт
+    // ждём ответа. по сигналу о готовности принятых данных или по таймауту, вывалимся из цикла
+    ret=loop.exec();
+    if(ret) goto stop; // если не ноль (ошибка таймаута) - вывалиться из режима. если 0, то приняли данные из порта
+
+    // !!! если нечего проверять, список пуст - проверить алгоритм
+    // Пробежимся по списку цепей
+    for(int i=1; i < modelClosedCircuitVoltageGroup->rowCount(); i++)
+    {
+        QStandardItem *sitm = modelClosedCircuitVoltageGroup->item(i, 0);
+        Qt::CheckState checkState = sitm->checkState();
+        if (checkState != Qt::Checked) continue;
+
+        // собрать режим
+        str_num.sprintf(" %02i", i); // напечатать номер цепи
+        baSendArray=(baSendCommand="UccG")+str_num.toLocal8Bit()+"#";
+        if(bDeveloperState) Log(QString("Sending ") + qPrintable(baSendArray), "blue");
+        QTimer::singleShot(delay_after_IDLE_before_other, this, SLOT(sendSerialData()));
+        ret=loop.exec();
+        if(ret) goto stop;
+
+        starttime = QDateTime::currentDateTime(); // время начала измерения
+        // опросить
+        baSendArray=baSendCommand+"?#";
+        QTimer::singleShot(delay_command_after_start_before_request, this, SLOT(sendSerialData()));
+        ret=loop.exec();
+        if(ret) goto stop;
+        codeADC = getRecvData(baRecvArray); // напряжение в коде
+        fU = ((codeADC-settings.offsetADC1)*settings.coefADC1); // напряжение в вольтах
+
+        // нарисуем график
+        dt = QDateTime::currentDateTime();
+        x= -dt.secsTo(starttime);
+        ui->widgetClosedCircuitVoltageGroup->graph(0)->clearData();
+        ui->widgetClosedCircuitVoltageGroup->graph(0)->rescaleValueAxis(true); // для чего? узнать.
+        ui->widgetClosedCircuitVoltageGroup->graph(0)->addData((double)x, (double)fU);
+        ui->widgetClosedCircuitVoltageGroup->replot();
+
+        while(-dt.msecsTo(starttime) < cycleTimeSec*1000) // пока время цикла проверки не вышло, продолжим измерять
+        {
+            // опросить
+            baSendArray=baSendCommand+"?#";
+            QTimer::singleShot(delay_command_after_request_before_next, this, SLOT(sendSerialData()));
+            ret=loop.exec();
+            if(ret) goto stop;
+            codeADC = getRecvData(baRecvArray); // напряжение в коде
+            fU = ((codeADC-settings.offsetADC1)*settings.coefADC1); // напряжение в вольтах
+            // нарисуем график
+            dt = QDateTime::currentDateTime();
+            x= -dt.msecsTo(starttime);// .secsTo(starttime);
+            ui->widgetClosedCircuitVoltageGroup->graph(0)->rescaleValueAxis(true);
+            ui->widgetClosedCircuitVoltageGroup->graph(0)->addData((double)x/1000, (double)fU);
+            ui->widgetClosedCircuitVoltageGroup->replot();
+        }
+
+        if(bDeveloperState)
+            Log("Цепь "+battery[iBatteryIndex].circuitgroup[i-1]+" Receive "+qPrintable(baRecvArray)+" codeADC1=0x"+QString("%1").arg((ushort)codeADC, 0, 16), "blue");
+
+        QLabel * label = findChild<QLabel*>(tr("labelClosedCircuitVoltageGroup%1").arg(i));
+        label->setText(tr("%1) %2").arg(i).arg(QString::number(fU)));
+        str = tr("%1) между контактом 1 соединителя Х3 «Х3-» и контактом %1 соединителя Х4 «4» = <b>%2</b>").arg(i).arg(QString::number(fU));
+        Log(str, (fU < settings.closecircuitgroup_limit) ? "red" : "green");
+        // проанализировать результаты
+        if(codeADC >= codeLimit) // напряжение больше (норма)
+        {
+            Log("Напряжение цепи "+battery[iBatteryIndex].circuitgroup[i-1]+" = "+QString::number(fU, 'f', 2)+" В.  Норма.", "blue");
+            // если ручной режим, то выдать окно сообщения, и только потом разобрать режим измерения.
+            if(bModeManual) QMessageBox::information(this, tr("Напряжение замкнутой цепи группы"), tr("Напряжение цепи ")+battery[iBatteryIndex].circuitgroup[i-1]+" = "+QString::number(fU, 'f', 2)+" В\nНорма");
+        }
+        else // напряжение меньше (не норма)
+        {
+            Log("Напряжение цепи "+battery[iBatteryIndex].circuitgroup[i-1]+" = "+QString::number(fU, 'f', 2)+" В.  Не норма!.", "red");
+            // если ручной режим, то выдать окно сообщения, и только потом разобрать режим измерения.
+            if(bModeManual) QMessageBox::information(this, tr("Напряжение замкнутой цепи группы"), tr("Напряжение цепи ")+battery[iBatteryIndex].circuitgroup[i-1]+" = "+QString::number(fU, 'f', 2)+" В\nНе норма!");
+            // !!! добавить цепь в список распассивируемых
+            //int rett = QMessageBox::question(this, "Внимание - "+ui->rbClosedCircuitVoltageGroup->text(), tr("%1 \nпродолжить?").arg(str), tr("Да"), tr("Да, необходима \"Распассивация\""), tr("Нет"));
+            switch (QMessageBox::question(this, "Внимание - "+ui->rbClosedCircuitVoltageGroup->text(), tr("%1 \nпродолжить?").arg(str), tr("Да"), tr("Да, необходима \"Распассивация\""), tr("Нет"))) {
+            case 0:
+                break;
+            case 1:
+                imDepassivation.append(iStepClosedCircuitVoltageGroup-1);
+                Log(tr("%1) %1 - Х4 «4» добавлен для распассивации.").arg(iStepClosedCircuitVoltageGroup-1), "blue");
+                break;
+            case 2:
+                ui->btnClosedCircuitVoltageGroup_2->setEnabled(true);
+                bPause = true;
+                return; // !!! тут как-то надо выйти из цикла, чтобы разобрать режим
+                break;
+            default:
+                break;
+            }
+            ui->rbModeDiagnosticManual->setChecked(true);
+            ui->rbModeDiagnosticAuto->setEnabled(false);
+        }
+
+        // разобрать режим
+        baSendArray = (baSendCommand="IDLE")+"#";
+        QTimer::singleShot(delay_command_after_request_before_next, this, SLOT(sendSerialData()));
+        ret=loop.exec();
+        if(ret) goto stop;
+    }// конец цикла проверок цепей
+stop:
+    // если отладочный режим, напечатать отладочную инфу
+    if(bDeveloperState)
+    {
+        if(ret==1) Log(tr("Timeout!"), "red");
+        else if(ret==2) Log(tr("Incorrect reply!"), "red");
+    }
+
+    timerPing->start(delay_timerPing); // запустить пинг по выходу из режима
+    baSendArray.clear(); // очистить буфера команд.
+    baSendCommand.clear();
+    baRecvArray.clear();
+
+    Log(tr("Проверка завершена - %1").arg(ui->rbClosedCircuitVoltageGroup->text()), "blue");
+    iStepClosedCircuitVoltageGroup = 1;
+    ui->rbClosedCircuitVoltageBattery->setEnabled(true);
+    ui->groupBoxCOMPort->setEnabled(true);
+    ui->groupBoxDiagnosticDevice->setEnabled(true);
+    ui->groupBoxDiagnosticMode->setEnabled(true);
+}
+#if 0
+switch (iBatteryIndex) {
+case 0: //9ER20P-20 // !!! ваще кейс не нужен, кол-во цепей брать из структуры.
+    ui->progressBar->setValue(iStepClosedCircuitVoltageGroup-1);
+    ui->progressBar->setMaximum(20); // !!! кол-во проверяемых цепей зависит от кол-ва установленных чекбоксов (и от неисправных без нагрузки цепей, допустим)
+    while (iStepClosedCircuitVoltageGroup <= 20) {
+        if (bPause) return;
+        switch (iStepClosedCircuitVoltageGroup) {
+        case 1:
+            delay(1000);
+            param = randMToN(26, 29); //число полученное с COM-порта
+            break;
+        case 2:
+            delay(1000);
+            param = randMToN(26, 29); //число полученное с COM-порта
+            break;
+        case 3:
+            delay(1000);
+            param = randMToN(26, 29); //число полученное с COM-порта
+            break;
+        case 4:
+            delay(1000);
+            param = randMToN(26, 29); //число полученное с COM-порта
+            break;
+        case 5:
+            delay(1000);
+            param = randMToN(26, 29); //число полученное с COM-порта
+            break;
+        case 6:
+            delay(1000);
+            param = randMToN(26, 29); //число полученное с COM-порта
+            break;
+        case 7:
+            delay(1000);
+            param = randMToN(26, 29); //число полученное с COM-порта
+            break;
+        case 8:
+            delay(1000);
+            param = randMToN(26, 29); //число полученное с COM-порта
+            break;
+        case 9:
+            delay(1000);
+            param = randMToN(26, 29); //число полученное с COM-порта
+            break;
+        case 10:
+            delay(1000);
+            param = randMToN(26, 29); //число полученное с COM-порта
+            break;
+        case 11:
+            delay(1000);
+            param = randMToN(26, 29); //число полученное с COM-порта
+            break;
+        case 12:
+            delay(1000);
+            param = randMToN(26, 29); //число полученное с COM-порта
+            break;
+        case 13:
+            delay(1000);
+            param = randMToN(26, 29); //число полученное с COM-порта
+            break;
+        case 14:
+            delay(1000);
+            param = randMToN(26, 29); //число полученное с COM-порта
+            break;
+        case 15:
+            delay(1000);
+            param = randMToN(26, 29); //число полученное с COM-порта
+            break;
+        case 16:
+            delay(1000);
+            param = randMToN(26, 29); //число полученное с COM-порта
+            break;
+        case 17:
+            delay(1000);
+            param = randMToN(26, 29); //число полученное с COM-порта
+            break;
+        case 18:
+            delay(1000);
+            param = randMToN(26, 29); //число полученное с COM-порта
+            break;
+        case 19:
+            delay(1000);
+            param = randMToN(26, 29); //число полученное с COM-порта
+            break;
+        case 20:
+            delay(1000);
+            param = randMToN(26, 29); //число полученное с COM-порта
+            break;
+        default:
+            return;
+            break;
+        }
+        //ui->widgetClosedCircuitVoltageGroup->graph(0)->clearData();
+        ui->widgetClosedCircuitVoltageGroup->graph(0)->rescaleValueAxis(true);
+        ui->widgetClosedCircuitVoltageGroup->graph(0)->addData(h*(iStepClosedCircuitVoltageGroup-1), param);
+        //ui->widgetClosedCircuitVoltageGroup->graph(1)->clearData();
+        //ui->widgetClosedCircuitVoltageGroup->graph(1)->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssCircle, Qt::green, Qt::white, 7));
+        /*if (param < settings.closecircuitgroup_limit) {
+            QPen gridPen;
+            gridPen.setStyle(Qt::DotLine);
+            gridPen.setColor(QColor(255, 0, 0, 25));
+            ui->widgetClosedCircuitVoltageGroup->graph(1)->setPen(gridPen);
+        }*/
+        //ui->widgetClosedCircuitVoltageGroup->graph(1)->addData(h*(iStepClosedCircuitVoltageGroup-1), param);
+        ui->widgetClosedCircuitVoltageGroup->replot();
+
+        QLabel * label = findChild<QLabel*>(tr("labelClosedCircuitVoltageGroup%1").arg(iStepClosedCircuitVoltageGroup));
+        label->setText(tr("%1) %2").arg(iStepClosedCircuitVoltageGroup).arg(QString::number(param)));
+        str = tr("%1) между контактом 1 соединителя Х3 «Х3-» и контактом %1 соединителя Х4 «4» = <b>%2</b>").arg(iStepClosedCircuitVoltageGroup).arg(QString::number(param));
+        Log(str, (param < settings.closecircuitgroup_limit) ? "red" : "green");
+        if (param < settings.closecircuitgroup_limit) {
+            int ret = QMessageBox::question(this, "Внимание - "+ui->rbClosedCircuitVoltageGroup->text(), tr("%1 \nпродолжить?").arg(str), tr("Да"), tr("Да, необходима \"Распассивация\""), tr("Нет"));
+            switch (ret) {
+            case 0:
+                break;
+            case 1:
+                imDepassivation.append(iStepClosedCircuitVoltageGroup-1);
+                Log(tr("%1) %1 - Х4 «4» добавлен для распассивации.").arg(iStepClosedCircuitVoltageGroup-1), "blue");
+                break;
+            case 2:
+                ui->btnClosedCircuitVoltageGroup_2->setEnabled(true);
+                bPause = true;
+                return;
+                break;
+            default:
+                break;
+            }
+            ui->rbModeDiagnosticManual->setChecked(true);
+            ui->rbModeDiagnosticAuto->setEnabled(false);
+            //ui->rbInsulationResistance->setChecked(true);
+            /*if (QMessageBox::question(this, "Внимание - "+ui->rbClosedCircuitVoltageGroup->text(), tr("%1 \nпродолжить?").arg(str), tr("Да"), tr("Нет"))) {
+                ui->btnClosedCircuitVoltageGroup_2->setEnabled(true);
+                bPause = true;
+                return;
+            }*/
+        }
+        progressBarSet(1);
+        iStepClosedCircuitVoltageGroup++;
+    }
+    if (imDepassivation.count() != 0)
+        ui->rbDepassivation->setEnabled(true);
+    ui->btnClosedCircuitVoltageGroup_2->setEnabled(false);
+    if (ui->rbModeDiagnosticAuto->isChecked())
+        bCheckCompleteClosedCircuitVoltageGroup = true;
+    break;
+case 1:
+    if (bPause) return;
+    Log("Действия проверки.", "green");
+    delay(1000);
+    progressBarSet(1);
+    break;
+case 2:
+    if (bPause) return;
+    Log("Действия проверки.", "green");
+    delay(1000);
+    progressBarSet(1);
+    break;
+case 3:
+    if (bPause) return;
+    Log("Действия проверки.", "green");
+    delay(1000);
+    progressBarSet(1);
+    break;
+default:
+    break;
+}
+#endif
